5024da4ca3623e3dd4b66f7b1bd9322c97fe3efe
Keisuke Kamahori <kamahori@cs.washington.edu>
2025-09-29 22:53:07 +0000

    refactor: rename vox-serve -> vox_serve

---
+++ vox_serve/__init__.py
@@ -0,0 +1,13 @@
+"""VoxServe: A serving system for Speech Language Models."""
+
+__version__ = "0.0.1"
+__author__ = "VoxServe Team"
+__description__ = "A serving system for speech language models."
+
+# Import modules will be available when package is properly installed
+
+__all__ = [
+    "__version__",
+    "__author__", 
+    "__description__",
+]
